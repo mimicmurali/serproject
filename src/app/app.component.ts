86e9008f21ad2c9fc128441b9b197679e9acec73
mimicmurali <r.muraliravi11@gmail.com>
2022-05-25 13:32:39 +0530

    secondcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public dService:DataService){
+
+  }
   title = 'serproject';
+  webdata:any;
+
+
+  fetchData(){
+    this.dService.getDataFromWebApi().subscribe(rcvdData=>{this.webdata=rcvdData;})
+  }
 }
